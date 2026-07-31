62b32374edbf6f7b0e58e878d98d193b05b9e351
Ashish Dahal <itsashishdahal@gmail.com>
2026-07-31 17:29:18 +0000

    Play Again

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import random
-user = input(' Pick one: \n Rock/Paper/Scissor :').title()
+
 list_1 = ['Rock', 'Paper', 'Scissor']
-computer = random.choice(list_1)
-print('The computer picked: ', computer)
-if user == 'Rock' and computer == 'Paper' or user == 'Paper' and computer == 'Scissor' or user == 'Scissor' and computer == 'Rock':
-    print('I won!  ')
-elif user == 'Rock' and computer == 'Scissor' or user == 'Paper' and computer == 'Rock' or user == 'Scissor' and computer == 'Paper':
-    print('You won!')
-elif user == computer:
-    print('We both chose the same!')
-else:
-    print("Invalid Entry")
+
+
+def the_game():
+    user = input(' Pick one: \n Rock/Paper/Scissor :').title()
+
+    if user not in list_1:
+        print('Invalid Entry!')
+    else:
+        computer = random.choice(list_1)
+        print('The computer picked: ', computer)
+
+        if (
+            (user == 'Rock' and computer == 'Paper') or
+            (user == 'Paper' and computer == 'Scissor') or
+            (user == 'Scissor' and computer == 'Rock')
+        ):
+            print('You lost!  ')
+
+        elif (
+            (user == 'Rock' and computer == 'Scissor') or
+            (user == 'Paper' and computer == 'Rock') or
+            (user == 'Scissor' and computer == 'Paper')
+        ):
+            print('You won!')
+        elif user == computer:
+            print('We both chose the same!')
+
+
+next_round = 'y'
+while next_round == 'y':
+    the_game()
+    next_round = input('Do you want to play the game again?:(Y/N):  ').lower()
