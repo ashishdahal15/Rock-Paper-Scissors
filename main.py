c9ccf9d7c5cd64e19c9b3f61393f43b504526298
Ashish Dahal <itsashishdahal@gmail.com>
2026-07-31 15:18:14 +0000

    Compiled Argument

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ user = input(' Pick one: \n Rock/Paper/Scissor :').title()
 list_1 = ['Rock', 'Paper', 'Scissor']
 computer = random.choice(list_1)
 print('The computer picked: ', computer)
-if user == 'Rock' and computer == 'Paper':
-    print('I won! Let me cover your rock! ')
-elif user == 'Rock' and computer == 'Scissor':
-    print('You won! Hit my Scissors!')
-elif user == 'Paper' and computer == 'Rock':
-    print('You won! Cover my Rock!')
-elif user == 'Paper' and computer == 'Scissor':
-    print('I won! Let me cut through you!')
-elif user == 'Scissor' and computer == 'Rock':
-    print('I won! Let me hit your scissor!')
-elif user == 'Scissor' and computer == 'Paper':
-    print('You won! Cut my paper!')
+if user == 'Rock' and computer == 'Paper' or user == 'Paper' and computer == 'Scissor' or user == 'Scissor' and computer == 'Rock':
+    print('I won!  ')
+elif user == 'Rock' and computer == 'Scissor' or user == 'Paper' and computer == 'Rock' or user == 'Scissor' and computer == 'Paper':
+    print('You won!')
 elif user == computer:
     print('We both chose the same!')
 else:
